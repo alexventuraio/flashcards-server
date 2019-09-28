ffeb6c97bf6ec32db97cfb75596abd2b1af1cdef
Alejandro Ventura Contreras <soyalexventura@gmail.com>
2019-09-28 12:02:16 -0500

    Configure some model validations and the corresponding relationships N:M.

--- app/models/flashcard.rb
+++ app/models/flashcard.rb
@@ -1,3 +1,8 @@
 class Flashcard < ApplicationRecord
   belongs_to :user
+  has_many :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
+
+  validates :question, presence: true
+  validates :answer, presence: true
 end
